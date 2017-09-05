b6bf0083ff013d02d098009f4325c061c0cb5f7a
devnalwa <nalwadev@grinnell.edu>
2017-09-04 19:50:54 -0500

    First Commit

---
+++ CaesarCipher.java
@@ -0,0 +1,94 @@
+import java.util.*;
+
+class CaesarCipher
+{
+
+	public static void main(String ar[])
+	{
+		String choice; 
+		String encode = "encode";
+		String decode = "decode";
+		Scanner in = new Scanner(System.in);
+		System.out.println("This program encrypts and decrypts messages using the Caeser Cipher.");
+		System.out.printf("Would you like to encode or decode a message? ");
+		choice = in.nextLine();
+		if(choice.equals(encode)) // used to string compare. 
+		{
+			encryptcaesar(); // calling encode function.
+		}
+		else if(choice.equals(decode))
+		{
+			decryptcaesar(); // calling decode function. 
+		}
+		else
+		{
+			System.out.println("Invalid input! Enter either 'encode' or 'decode' "); 
+		}
+
+	}
+
+	public static void encryptcaesar() // this method encodes a given string from a user using a specific formula. 
+	{
+		int base = 97; // declaration of base to keep track of ascii value. 
+		Scanner in = new Scanner(System.in);
+		String en ;
+		System.out.printf("Enter the string to encode: ");
+		en = in.nextLine();
+       for(int n = 0; n<26; n++) // loop to display all 26 different possibilities.
+        {
+            System.out.printf("n =" +n+ ":  ");
+            char[] fin = new char[en.length()]; // array creation to hold the new encoded word.
+            for(int i=0;i<en.length();i++)
+            {
+
+                char temp = en.charAt(i);
+                int asc = ((((int) temp) - base) + n) ;
+                if(asc>25){ // check if value needs to be wrapped around.
+                     asc= (asc-26) + base;
+                }
+                else{
+                     asc = asc+base ;
+                }
+                char f = (char) asc ;
+                fin[i] = f;
+            }
+            String finalans = new String(fin);
+            System.out.println(finalans); // printing new string
+        }
+
+	}
+	public static void decryptcaesar() // this method decodes a given string from a user using a specific formula.
+	{
+		int base = 97; // declaration of base to keep track of ascii value. 
+		Scanner in = new Scanner(System.in);
+		String en ;
+		System.out.printf("Enter the string to decode: ");
+		en = in.nextLine();
+		
+		for(int n = 0; n<26; n++)
+        {
+            System.out.printf("n =" +n+ ":  ");
+            char[] fin = new char[en.length()];
+            for(int i=0;i<en.length();i++)
+            {
+
+                char temp = en.charAt(i);
+                int asc = ((((int) temp) - base) - n) ;
+                if(asc<0)
+                {
+                     asc= (asc+26) + base;
+                }
+                else
+                {
+                     asc = asc+base ;
+                }
+                char f = (char) asc ;
+                fin[i] = f;
+            }
+            String finalans = new String(fin);
+            System.out.println(finalans);
+        }
+   
+    }
+		
+}
---
+++ README.txt
@@ -0,0 +1,11 @@
+1) NAMES:
+	- Dev Nalwa
+	- Erhaan Ahmad
+2)
+	- This is the submission of the classical encryption homework. It contains two different cyphering methods: one is a caesar cipher and the other is a vigenere cipher.
+
+3)
+	- We did not use any outside help or resources other than what was provided to us in the homework prompt. 
+	
+	
+	
